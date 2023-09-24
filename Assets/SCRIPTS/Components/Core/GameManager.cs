ee86d20a7d9eac4be691cb2c219dcfe0048dad5d
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2023-09-24 21:39:13 +0200

    ~ WebGl Template
    ~ Modified the WebGl template.
    - The previous template didn't work.

--- Assets/SCRIPTS/Components/Core/GameManager.cs
+++ Assets/SCRIPTS/Components/Core/GameManager.cs
@@ -1,9 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace GGG.Components.Core
 {
-    public class GameManager : MonoBehaviour
-    {
-        
+    public class GameManager : MonoBehaviour {
+
+        #region Singleton
+
+        public static GameManager Instance;
+
+        private void Awake() {
+            if (Instance == null) Instance = this;
+            
+            if(!DebugMode) StartCoroutine(InitializeGame());
+        }
+
+        #endregion
+
+        [SerializeField] private bool DebugMode;
+
+        private IEnumerator InitializeGame() {
+            yield return null;
+            const int SCENE_IDX = 1;
+            
+            // TODO - Load Main Menu
+            // TEMP:
+            AsyncOperation async = SceneManager.LoadSceneAsync(SCENE_IDX, LoadSceneMode.Additive);
+            while (!async.isDone) yield return null;
+        }
     }
 }
