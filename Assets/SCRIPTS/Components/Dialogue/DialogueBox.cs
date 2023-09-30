431360208814f12494c14f7ba074c64e17a380b8
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2023-09-30 15:50:16 +0200

    + Improvements
    - Different names and avatars can be display

--- Assets/SCRIPTS/Components/Dialogue/DialogueBox.cs
+++ Assets/SCRIPTS/Components/Dialogue/DialogueBox.cs
@@ -6,20 +6,30 @@ using System.Collections;
 using System.Text;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace GGG.Components.Dialogue
 {
     public class DialogueBox : MonoBehaviour {
+        [Header("Dialogue")]
         [SerializeField] [TextArea] private List<string> DialogueLines;
-        [SerializeField] private List<Image> CharacterExpressions;
         [SerializeField] private float TypingSpeed = 0.1f;
+
+        [Space(5)] 
+        [Header("Other Fields")] 
+        [SerializeField] private List<Sprite> AvatarList;
+        [SerializeField] private Image Avatar;
+        [SerializeField] private List<string> CharacterNames;
+        [SerializeField] private TMP_Text NameText;
 
         private InputManager _input;
         private TMP_Text _text;
         private CanvasGroup _canvasGroup;
         
         private int _lineIdx;
+        private int _avatarIdx;
+        private int _characterIdx;
         private bool _started;
         private bool _finished;
         private bool _currentTextFinished;
@@ -39,6 +49,11 @@ namespace GGG.Components.Dialogue
             
             _canvasGroup.alpha = 0f;
             _lineIdx = 0;
+            _avatarIdx = 0;
+            _characterIdx = 0;
+            
+            NameText.SetText(CharacterNames[_characterIdx]);
+            Avatar.sprite = AvatarList[_avatarIdx];
         }
 
         private void Update() {
@@ -60,14 +75,23 @@ namespace GGG.Components.Dialogue
             if (!_input.MouseClick()) return;
             
             if (_lineIdx < DialogueLines.Count) {
-                _currentTextFinished = false;
-                
-                if (!_currentTextFinished)
-                    StopAllCoroutines();
-                
+                if (!_currentTextFinished) {
+                    ShowAllText();
+                    return;
+                }
+
                 StartCoroutine(TypeText(DialogueLines[_lineIdx++]));
+                if(_avatarIdx < AvatarList.Count)
+                    Avatar.sprite = AvatarList[_avatarIdx++];
+                if(_characterIdx < CharacterNames.Count)
+                    NameText.SetText(CharacterNames[_characterIdx++]);
             }
             else {
+                if (!_currentTextFinished) {
+                    ShowAllText();
+                    return;
+                }
+                
                 OnDialogueEnd();
             }
         }
@@ -87,6 +111,7 @@ namespace GGG.Components.Dialogue
 
         private IEnumerator TypeText(string text) {
             _currentText = new StringBuilder();
+            _currentTextFinished = false;
             
             for (int i = 0; i < text.Length; i++) {
                 _currentText.Append(text[i]);
@@ -94,6 +119,12 @@ namespace GGG.Components.Dialogue
                 yield return new WaitForSeconds(TypingSpeed);
             }
             
+            _currentTextFinished = true;
+        }
+
+        private void ShowAllText() {
+            StopAllCoroutines();
+            _text.SetText(DialogueLines[_lineIdx - 1]);
             _currentTextFinished = true;
         }
     }
