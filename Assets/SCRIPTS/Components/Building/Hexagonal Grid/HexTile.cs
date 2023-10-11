de7c612c6b88ea04bd411e0913b474d92aba7d92
AdrianAlbarran <adrian-240102@hotmail.es>
2023-10-11 22:16:15 +0200

    Little work on sell button, lots of problems
    + Progress on sell button WIP
    + Added UI to scene, WIP
    ~ BIG Problem with how you interact with cell. More on that on Teams

--- Assets/SCRIPTS/Components/Building/Hexagonal Grid/HexTile.cs
+++ Assets/SCRIPTS/Components/Building/Hexagonal Grid/HexTile.cs
@@ -79,7 +79,7 @@ namespace GGG.Components.Buildings {
         }
 
         public void AddTile() {
-            tilePrefab = Instantiate(settings.GetTile(tileType), transform.position, Quaternion.Euler(-90f, 0f, 0f), transform);
+            tilePrefab = Instantiate(settings.GetTile(tileType), transform.position, Quaternion.Euler(0f, 0f, 0f), transform);
             if (_manager) {
                 _highlightPrefab = Instantiate(_manager.highlightPrefab, transform.position, Quaternion.Euler(-90f, 0f, 0f), transform);
                 _highlightPrefab.SetActive(false);
--- Assets/SCRIPTS/Components/UI/Buttons/SellButton.cs
+++ Assets/SCRIPTS/Components/UI/Buttons/SellButton.cs
@@ -1,9 +1,32 @@
+using GGG.Components.Buildings;
+using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace GGG.Components.UI {
     public class SellButton : InteractableButton {
-        public override void OnPointerDown(PointerEventData eventData) {
-            
+
+        private HexTile _selectedHexTile;
+        public Action onStructureSold;
+
+
+        void Start()
+        {   
+            HexTile[] hexTiles = FindObjectsOfType<HexTile>();
+
+            foreach (HexTile tile in hexTiles)
+            {
+                tile.OnHexSelect += () => {
+                    _selectedHexTile = tile;
+
+                    print(_selectedHexTile.name);
+                    print(_selectedHexTile.TileEmpty());
+                    if (!_selectedHexTile.TileEmpty())
+                    {
+                        print(tile.name);
+                    }
+                };
+            }
         }
     }
 }
