e4e67534f9b245515a7b9c52e0abf6a33a331a9f
AdrianAlbarran <adrian-240102@hotmail.es>
2023-10-25 19:57:17 +0200

    Revert "Merge pull request #28 from GluGluGames/SolverMerge"
    This reverts commit c3257eae68ca0cbf5028bf9587f2bf270195cafe, reversing
    changes made to f691e78dc8d281efb5eecf50694ad2092df6121e.

--- Assets/SCRIPTS/Components/Enemies/Enemy.cs
+++ Assets/SCRIPTS/Components/Enemies/Enemy.cs
@@ -1,65 +1,52 @@
-using GGG.Components.Buildings;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace GGG.Components.Enemies
+public abstract class Enemy : MonoBehaviour
 {
-    public abstract class Enemy : MonoBehaviour
-    {
-        [Header("Generic fields")]
-        [Tooltip("Name of the enemy")]
-        [SerializeField] private string Name;
 
-        [Tooltip("Description of the enemy")]
-        [SerializeField][TextArea] private string Description;
+    [Header("Generic fields")]
+    [Tooltip("Name of the enemy")]
+    [SerializeField] private string Name;
+    [Tooltip("Description of the enemy")]
+    [SerializeField][TextArea] private string Description;
+    [Tooltip("Icon of the enemy. Used in the UI")]
+    [SerializeField] private Sprite Icon;
+    [Tooltip("Prefab of the enemy")]
+    [SerializeField] private GameObject Prefab;
+    [Space(10)]
+    [Header("Enemy Fields")]
+    [Tooltip("Determines the height of the enemy")]
+    [SerializeField] private float SpawnHeight;
 
-        [Tooltip("Icon of the enemy. Used in the UI")]
-        [SerializeField] private Sprite Icon;
+    /// <summary>
+    /// Interacts with the enemy
+    /// </summary>
+    public abstract void Interact();
 
-        [Tooltip("Prefab of the enemy")]
-        [SerializeField] protected GameObject Prefab;
+    /// <summary>
+    /// Gets the name of the enemy
+    /// </summary>
+    /// <returns>The name of the enemy</returns>
+    public string GetName() { return Name; }
 
-        [Space(10)]
-        [Header("Enemy Fields")]
-        [Tooltip("Determines the height of the enemy")]
-        [SerializeField] protected float SpawnHeight;
+    /// <summary>
+    /// Gets the description of the enemy
+    /// </summary>
+    /// <returns>The description of the enemy</returns>
+    public string GetDescription() { return Description; }
 
-        [SerializeField] protected int hp;
-        [SerializeField] protected bool isStoppable;
-        [SerializeField] protected bool _alwaysVisible;
+    /// <summary>
+    /// Gets the sprite of the enemy
+    /// </summary>
+    /// <returns>The sprite of the enemy</returns>
+    public Sprite GetIcon() { return Icon; }
 
-        protected Rigidbody Rb;
-        public HexTile currentTile;
-        public Vector3Int cubeCoordPos;
-        public bool isDirty = false;
-        
-        ///<summary>
-        /// Gets the name of the enemy
-        /// </summary>
-        /// <returns>The name of the enemy</returns>
-        public string GetName()
-        { return Name; }
+    /// <summary>
+    /// Spawns the enemy
+    /// </summary>
+    /// <param name="position">The position where the enemy will be spawned</param>
+    /// <returns>The instance of the enemy</returns>
+    public GameObject Spawn(Vector3 position) { return Instantiate(Prefab, new Vector3(position.x, SpawnHeight, position.z), Quaternion.Euler(0, 0, 0)); }
 
-        /// <summary>
-        /// Gets the description of the enemy
-        /// </summary>
-        /// <returns>The description of the enemy</returns>
-        public string GetDescription()
-        { return Description; }
-
-        /// <summary>
-        /// Gets the sprite of the enemy
-        /// </summary>
-        /// <returns>The sprite of the enemy</returns>
-        public Sprite GetIcon()
-        { return Icon; }
-
-        /// <summary>
-        /// Spawns the enemy
-        /// </summary>
-        /// <param name="position">The position where the enemy will be spawned</param>
-        /// <returns>The instance of the enemy</returns>
-        public GameObject Spawn(Vector3 position)
-        { return Instantiate(Prefab, new Vector3(position.x, SpawnHeight, position.z), Quaternion.Euler(0, 0, 0)); }
-
-    }
 }
---
+++ Assets/SCRIPTS/Components/Enemies/EnemyBasic.cs
@@ -0,0 +1,138 @@
+using GGG.Components.Buildings;
+using GGG.Components.Ticks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace GGG.Components.Enemies
+{
+    public class EnemyBasic : MonoBehaviour
+    {
+        [SerializeField] private GameObject _prefab;
+        [SerializeField] private int _hp;
+        [SerializeField] private bool _killable;
+        [SerializeField] private bool _stoppable;
+        [SerializeField] private bool _alwaysVisible;
+
+        private Rigidbody _rigidbody;
+
+        private bool _moving;
+        private bool _gotPath;
+
+        public HexTile currentTile;
+        public HexTile nextTile;
+        public Vector3 targetPosition;
+        public List<HexTile> currentPath = new List<HexTile>();
+        public Vector3Int cubeCoordPos;
+
+        void Start()
+        {
+            TickManager.OnTick += this.HandleMovement;
+            TickManager.OnTick += this.HandleVisibility;
+            _rigidbody = GetComponent<Rigidbody>();
+            HandleVisibility();
+        }
+
+        private void OnDisable()
+        {
+            TickManager.OnTick -= this.HandleMovement;
+            TickManager.OnTick -= this.HandleVisibility;
+
+        }
+
+        void Update()
+        {
+            if(!_gotPath)
+            {
+                GoToRandomTile();
+            }
+        }
+
+        private void GoToRandomTile()
+        {
+            HexTile destination = TileManager.instance.GetRandomHex();
+            targetPosition = destination.transform.position;
+            currentPath = Pathfinder.FindPath(currentTile, destination);
+            currentPath.Reverse();
+            if (currentPath != null ) { _gotPath = true; }
+            
+        }
+
+        private void MoveTo(Vector3 targetPos)
+        {
+            Quaternion angle = Quaternion.Euler(0, Vector3.Angle(targetPos, transform.forward), 0);
+            _rigidbody.Move(targetPos, angle);
+
+        }
+
+        private void HandleMovement()
+        {
+            if (currentPath == null || currentPath.Count <= 1)
+            {
+                nextTile = null;
+
+                if (currentPath != null && currentPath.Count > 0)
+                {
+                    currentTile = currentPath[0];
+                    nextTile = currentTile;
+                }
+
+                _gotPath = false;
+
+            }
+            else
+            {
+                currentTile = currentPath[0];
+
+                nextTile = currentPath[0];
+
+                // if the next tile is not traversable, stop moving;
+                /*if (PlayerPosition.NextTile.tileType != HexTileGenerationSettings.TileType.Standard)
+                {
+                    PlayerPosition.currentPath.Clear();
+                    HandleMovement();
+                    return;
+                }*/
+
+                targetPosition = nextTile.transform.position + new Vector3(0, 1f, 0);
+                MoveTo(targetPosition);
+                _gotPath = true;
+                currentPath.RemoveAt(0);
+                cubeCoordPos = nextTile.cubeCoordinate;
+            }
+        }
+
+        private void HandleVisibility()
+        {
+            // Prob this should be a FSM
+            if(nextTile != null && !_alwaysVisible)
+            {
+                if(nextTile.gameObject.layer == 0 || (currentTile != null && currentTile.gameObject.layer == 0))
+                {
+                    gameObject.layer = 8;
+                } else 
+                {
+                    gameObject.layer = 7;
+                }
+            } else if (currentTile != null && !_alwaysVisible)
+            {
+                if(currentTile.gameObject.layer == 0)
+                {
+                    gameObject.layer = 8;
+                }
+                else
+                {
+                    gameObject.layer = 7;
+                }
+            } else if(_alwaysVisible)
+            {
+                currentTile.gameObject.layer = 8;
+            }
+            
+
+        }
+    }
+
+}
+
