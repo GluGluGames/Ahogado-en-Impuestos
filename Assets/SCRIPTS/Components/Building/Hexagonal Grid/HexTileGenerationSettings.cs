00b25415490ce8e9f8ab77431521fb81a35d7e16
daniever6 <asd1148809533@gmail.com>
2023-11-05 12:07:16 +0100

    +First Map
    New map with old prefabs, need to decorate later.

--- Assets/SCRIPTS/Components/Building/Hexagonal Grid/HexTileGenerationSettings.cs
+++ Assets/SCRIPTS/Components/Building/Hexagonal Grid/HexTileGenerationSettings.cs
@@ -9,7 +9,7 @@ namespace GGG.Components.Buildings
         Standard,
         Water,
         Cliff,
-        Walkable
+        UnWalkable
     }
 
     [CreateAssetMenu(menuName = "TileGen/GenerationSettings")]
@@ -18,7 +18,7 @@ namespace GGG.Components.Buildings
         [SerializeField] private GameObject Standar;
         [SerializeField] private GameObject Water;
         [SerializeField] private GameObject Cliff;
-        [SerializeField] private GameObject Walkable;
+        [SerializeField] private GameObject UnWalkable;
 
         public GameObject GetTile(TileType tileType)
         {
@@ -27,7 +27,7 @@ namespace GGG.Components.Buildings
                 case TileType.Standard: return Standar;
                 case TileType.Water: return Water;
                 case TileType.Cliff: return Cliff;
-                case TileType.Walkable: return Walkable;
+                case TileType.UnWalkable: return UnWalkable;
             }
             return null;
         }
