e71bd6f897a3ae5ea8295e20b8ab5defadcca2dd
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2023-12-07 03:31:37 +0100

    + Added Colored Headers

---
+++ Assets/EXTERNAL/Baedrick/ColoredHeaderCreator/ColoredHeaderPreset.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plugins.Baedrick.ColoredHeaderCreator
+{
+	[System.Serializable]
+	[CreateAssetMenu(fileName = "New Header Preset", menuName = "Colored Header Creator/Header Preset")]
+	public class ColoredHeaderPreset : ScriptableObject
+	{
+		public List<HeaderSettings> coloredHeaderPreset = new List<HeaderSettings>();
+	}
+}
+
+#endif
