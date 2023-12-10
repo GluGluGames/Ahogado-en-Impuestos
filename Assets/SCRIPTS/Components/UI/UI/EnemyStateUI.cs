9a5fccc8358404f732c25598862ac91a797dedb8
AdrianAlbarran <adrian-240102@hotmail.es>
2023-12-10 22:15:37 +0100

    + visibility fixed

--- Assets/SCRIPTS/Components/UI/UI/EnemyStateUI.cs
+++ Assets/SCRIPTS/Components/UI/UI/EnemyStateUI.cs
@@ -17,8 +17,37 @@ namespace GGG.Components.UI
     public class EnemyStateUI : MonoBehaviour
     {
         [SerializeField] private List<Image> ImageList = new List<Image>();
+        GameObject father;
+        [SerializeField] private List<GameObject> visibilityListy;
+
 
         private StateIcon currentState;
+
+        private void Start()
+        {
+            father = transform.parent.gameObject;
+        }
+
+        private void Update()
+        {
+            if (father.layer == 7)
+            {
+                transform.gameObject.layer = 7;
+                foreach (GameObject go in visibilityListy)
+                {
+                    go.layer = 7;
+                }
+            }
+            else
+            {
+                transform.gameObject.layer = 8;
+                foreach (GameObject go in visibilityListy)
+                {
+                    go.layer = 8;
+                }
+            }
+            
+        }
 
         public StateIcon GetCurrentState()
         {
